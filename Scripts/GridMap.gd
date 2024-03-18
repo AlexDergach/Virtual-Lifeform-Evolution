35e78b05710c05d549cc0169c30ca81e05f510ca
AlexDergach <dergachalex12@gmail.com>
2024-03-18 20:30:58 +0000

    Made Ocean Infinite

--- Scripts/GridMap.gd
+++ Scripts/GridMap.gd
@@ -5,6 +5,7 @@ var Dic = {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	#get_cell_item(,1)
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
