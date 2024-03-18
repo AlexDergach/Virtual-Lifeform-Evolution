56ba8281cb8141cf9ec2df5797a8373a862b96c7
AlexDergach <dergachalex12@gmail.com>
2024-03-18 22:29:30 +0000

    Added Better FOV and Speed to the camera

--- Scripts/Camera.gd
+++ Scripts/Camera.gd
@@ -2,6 +2,10 @@ extends CharacterBody3D
 
 var speed
 var cameraRotaion = 0.005
+var default_fov = 80
+var sprint_fov_increase = 15
+var fov_change_speed = 50.0
+
 
 # For pivot of Camera
 @onready var head = $Camera
@@ -15,6 +19,8 @@ var descend_speed = 5.0
 var last_jump_time = 0.0
 var double_jump_threshold = 500  # milliseconds
 
+var sprinting = false
+
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	set_process(true)
@@ -25,7 +31,6 @@ func _unhandled_input(event):
 		camera.rotate_x(-event.relative.y * cameraRotaion)
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
 	elif event is InputEventKey and event.pressed:
-		# Toggle flying when the space bar is pressed
 		if Input.is_action_just_pressed("Jump"):
 			var current_time = Time.get_ticks_msec()
 			if current_time - last_jump_time < double_jump_threshold:
@@ -34,20 +39,46 @@ func _unhandled_input(event):
 		elif Input.is_action_just_pressed("Ctrl"):
 			velocity.y = -descend_speed
 		elif Input.is_action_just_pressed("Sprint"):
+			sprinting = true
 			speed = 9.0
-		
+			camera.fov = default_fov + sprint_fov_increase
+
+	elif event is InputEventKey and not event.pressed:
+		if Input.is_action_just_released("Sprint"):
+			sprinting = false
 
 func _physics_process(delta):
+	var target_fov: float
+
+	if sprinting:
+		target_fov = default_fov + sprint_fov_increase
+	else:
+		target_fov = default_fov
+
+	if camera.fov != target_fov:
+		var fov_change = fov_change_speed * delta
+		if camera.fov < target_fov:
+			camera.fov = min(camera.fov + fov_change, target_fov)
+		else:
+			camera.fov = max(camera.fov - fov_change, target_fov)
+	
+	
 	if not flying and not is_on_floor():
 		velocity.y -= 7 * delta 
 
 	if not flying and Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = 5.0
 
-	if Input.is_action_pressed("Sprint"):
-		speed = 9.0
+	if flying:
+		if Input.is_action_pressed("Sprint"):
+			speed = 140.0
+		else:
+			speed = 5.0
 	else:
-		speed = 5.0
+		if Input.is_action_pressed("Sprint"):
+			speed = 9.0
+		else:
+			speed = 5.0
 
 	var input_dir = Input.get_vector("Left", "Right", "Forward", "Back")
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
@@ -76,16 +107,10 @@ func _physics_process(delta):
 		velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
 		velocity.z = lerp(velocity.z, direction.z * speed, delta * 3.0)
 
-	var velocity_clamped = clamp(velocity.length(), 0.5, fly_speed * 2)
-	var fov = 60 + 4 * velocity_clamped
-	camera.fov = lerp(camera.fov, fov, delta * 8.0)
-
 	move_and_slide()
-	
+
 func _process(delta):
 	ocean.position.x = self.position.x
 	ocean.position.z = self.position.z
-	# Esc key to quit game
 	if Input.is_action_pressed("Exit"):
 		get_tree().quit()
-
