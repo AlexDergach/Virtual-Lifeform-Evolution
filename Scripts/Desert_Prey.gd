933c53be3e0a9dad91236a09ca4a8d15a4e6b8a7
AlexDergach <dergachalex12@gmail.com>
2024-03-31 22:04:21 +0100

    Working on repo states with males and female logic

--- Scripts/Desert_Prey.gd
+++ Scripts/Desert_Prey.gd
@@ -3,8 +3,11 @@ extends CharacterBody3D
 @onready var nav: NavigationAgent3D = $NavigationAgent3D
 @onready var progress_bar = $SubViewport/Hunger
 @onready var progress_bar2 = $SubViewport/Repo
+@onready var progress_bar3 = $SubViewport/Gender
+
 @onready var progress_bar_text = $SubViewport/RichTextLabel
 @onready var progress_bar_text2 = $SubViewport/RichTextLabel2
+@onready var progress_bar_text3 = $SubViewport/RichTextLabel3
 
 
 
@@ -18,7 +21,7 @@ var time:float = 0.0
 var metabolism
 var size
 var hunger
-var reproduction
+var reproduction = 1
 
 var hunt = true
 var done
@@ -32,6 +35,7 @@ var food_location:Vector3 = Vector3.ZERO
 var direction = Vector3()
 var accel
 var speed
+var inital_speed
 
 var hunger_label: Label3D
 var reproduction_label: Label3D
@@ -44,6 +48,8 @@ var child_duration = 10.0
 var child_speed_factor = 0.75
 var child_scale_factor = 0.25
 
+var is_female = false  # Default to false
+
 
 func _ready():
 	
@@ -55,22 +61,42 @@ func _ready():
 		# If this instance is a child, start the timer for transitioning parameters
 		$Child_Timer.start()
 		# Scale down the size
-		size *= child_scale_factor
-		# Reduce the speed
-		speed *= child_speed_factor
+		#size *= child_scale_factor
+		self.scale = Vector3(size,size,size)
+		inital_speed = speed
+		# Do hunger
 	else:
 		# If it's not a child (i.e., an adult), initialize random size, speed, and hunger capacity
-		size = randf_range(3.0, 5.0)
-		accel = randf_range(3.0, 5.0)
-		speed = randf_range(1.0, 3.0)  # Adjust as needed
-		hunger = randf_range(1.0, 5.0)  # Adjust as needed
+		size = randf_range(1.0, 2.0)
+		self.scale = Vector3(size,size,size)
+		accel = randi_range(3.0, 5.0)
+		speed = randi_range(1.0, 3.0)  # Adjust as needed
+		inital_speed = speed
+		hunger = randi_range(6.0, 12.0)  # Adjust as needed
 		metabolism = size / 2
-		print(" Size: ", size , " Accel: ", accel," Speed: ",speed, " Hunger: ", hunger, " Meta: ", metabolism)
+		is_female = randf() < 1.0 / 3.0   # Randomly assign true (female) or false (male)
+		
+		print(" Size: ", size , " Accel: ", accel," Speed: ",speed, " Hunger: ", 
+		hunger, " Meta: ", metabolism, " Female: ", is_female)
 
 		
 	progress_bar.min_value = 0
 	progress_bar2.max_value = 1
 	progress_bar2.min_value = 0
+	progress_bar3.value = progress_bar3.max_value
+	
+	if is_female:
+		print("Female")
+		var desired_color = Color(1.0, 0.75, 0.8) # Red color
+		progress_bar3.modulate = desired_color
+		progress_bar_text3.text = "Female"
+		
+	else:
+		print("Male")
+		
+		var desired_color = Color(0.5, 0.5, 1.0) # Red color
+		progress_bar3.modulate = desired_color
+		progress_bar_text3.text = "Male"
 
 
 func _process(delta):
@@ -83,6 +109,11 @@ func _process(delta):
 	if progress_bar.value > 0:
 		progress_bar.value = hunger
 		progress_bar_text.text = "Food : " + str(hunger)
+		
+	if hunger > 0.0:
+		reproduction = 1
+	else: 
+		reproduction = 0
 			
 	if reproduction == 1:
 		progress_bar2.value = reproduction
@@ -90,11 +121,10 @@ func _process(delta):
 	else:
 		progress_bar2.value = reproduction
 		progress_bar_text2.text = " "
+	
+	
 		
-	if hunger > 0.0:
-		reproduction = 1
-	else: 
-		reproduction = 0
+
 
 func _physics_process(delta):
 	
@@ -136,9 +166,13 @@ func _on_timer_timeout():
 	
 var enemy = null
 var mating_partner = null
+var mating_partner_1 = null
+var mating_partner_2 = null
 var has_mated = false
 var first_mate_size
-var first_mate_speed
+var first_mate_hunger
+var partners = 0
+var mate_chosen = 1
 
 func _on_sensory_area_entered(area):
 	
@@ -152,7 +186,38 @@ func _on_sensory_area_entered(area):
 		enemy = area
 		$StateChart.send_event("enemy_entered")
 		time_since_last_target_update = randf_range(1.0, 10.0)  # Start running immediately
-	
+		
+	if is_female:		
+		if area.is_in_group("desert_prey") and !has_mated and !area.get_parent().is_female:
+			
+			if mating_partner_1 == null:
+				print("Partner 1 spotted")
+				mating_partner_1 = area
+				# Save the stats of the first encountered mate
+				first_mate_size = mating_partner_1.get_parent().size
+				first_mate_hunger = mating_partner_1.get_parent().hunger
+				print("Average Score 1: ", first_mate_size + first_mate_hunger)
+				partners += 1
+			elif mating_partner_2 == null and area != mating_partner_1:
+				print("Partner 2 spotted")
+				mating_partner_2 = area
+				# Compare stats with the second mate
+				var second_mate_size = area.get_parent().size
+				var second_mate_hunger = area.get_parent().hunger
+				print("Average Score 2: ", second_mate_size + second_mate_hunger)
+				
+				if second_mate_size + second_mate_hunger > first_mate_size + first_mate_hunger:
+					# Second mate is better, mate with it
+					mating_partner_2 = area
+					first_mate_size = second_mate_size
+					first_mate_hunger = second_mate_hunger
+					mate_chosen = 2
+				# else: Stick with the first mate
+				partners += 1
+				
+			if partners == 2:
+				print("Mating with : ", mate_chosen)
+
 func _on_self_area_entered(area):
 	if area.is_in_group("desert_pred"):
 		#print("Dead")
@@ -162,34 +227,15 @@ func _on_self_area_entered(area):
 		food_target = false
 		hunger += 1
 		#print("Prey: Food ate")
-		
-	if area.is_in_group("desert_prey") and !has_mated:
-		if mating_partner == null:
-			mating_partner = area
-			# Save the stats of the first encountered mate
-			first_mate_size = mating_partner.size
-			first_mate_speed = mating_partner.speed
-		else:
-			# Compare stats with the second mate
-			var second_mate_size = area.size
-			var second_mate_speed = area.speed
-			if second_mate_size + second_mate_speed > first_mate_size + first_mate_speed:
-				# Second mate is better, mate with it
-				mating_partner = area
-				first_mate_size = second_mate_size
-				first_mate_speed = second_mate_speed
-			# else: Stick with the first mate
-		
-		# Move to the mate for reproduction
-		nav.target_position = mating_partner.global_position
+	if area.is_in_group("desert_prey") and partners == 2 and area == mating_partner:
+		print("Mate: ", mate_chosen, " Touched Sending Repo State")
+		$StateChart.send_event("repo")
+	
 
 #If Pred Leaves The Sensory Area
 func _on_sensory_area_exited(area):
 	if area.is_in_group("desert_pred"):
 		$StateChart.send_event("enemy_exited")
-		
-	
-
 
 func _on_wandering_state_entered():
 	var num_escape_directions = 3
@@ -225,43 +271,68 @@ func _on_running_state_processing(delta):
 
 
 func _on_wandering_state_processing(delta):
-	var TARGET_UPDATE_INTERVAL = randf_range(5.0, 15.0)
+	var TARGET_UPDATE_INTERVAL = randf_range(4.0, 10.0)
 	time_since_last_target_update += delta
 	
-	if food_target == false:
-		# Continue wandering
-		if time_since_last_target_update >= TARGET_UPDATE_INTERVAL:
-			var random_dir = Vector3(randf_range(-0.5, 0.5), 0.1, randf_range(-0.5, 0.5)).normalized()
-			target_pos = global_position + Vector3(random_dir.x * roam_size, 0.1, random_dir.z * roam_size)
-			nav.target_position = target_pos
-			time_since_last_target_update = 0.0
-
+	if partners != 2:
+		if food_target == false:
+			# Continue wandering
+			if time_since_last_target_update >= TARGET_UPDATE_INTERVAL:
+				
+				var random_dir = Vector3(randf_range(-0.5, 0.5), 0.1, randf_range(-0.5, 0.5)).normalized()
+				target_pos = global_position + Vector3(random_dir.x * roam_size, 0.1, random_dir.z * roam_size)
+				nav.target_position = target_pos
+				time_since_last_target_update = 0.0
+				
+			# Check if the creature has reached its target position
+			if global_position.distance_to(nav.target_position) < 1.0: 
+				time_since_last_target_update = 20.0  # Reset the timer to find a new target position
+	else: 
+			if mate_chosen == 1:
+				print("Going to one")
+				mating_partner = mating_partner_1
+				nav.target_position = mating_partner_1.global_position
+			else: 
+				print("Going to two")
+				mating_partner = mating_partner_2
+				nav.target_position = mating_partner_2.global_position
+	
 
 func _on_repo_state_entered():
+	# Reproduction state entered
 	if mating_partner != null:
+		print("In Repo state")
 		# Mate with the partner
 		has_mated = true
-		var avg_size = (size + mating_partner.size) / 2.0
-		var avg_speed = (speed + mating_partner.speed) / 2.0
-		create_child(avg_size, avg_speed)
+		var avg_size = (size + mating_partner.get_parent().size) / 2.0
+		var avg_speed = (inital_speed + mating_partner.get_parent().inital_speed) / 2.0
+		var avg_accel = (accel + mating_partner.get_parent().accel) / 2.0
+		create_child(avg_size,avg_speed,avg_accel)
 		# Reset mating_partner for future reproduction
 		mating_partner = null
+		$StateChart.send_event("repo_done")
 
 
-func create_child(size, speed):
-	pass
-	# Create a new child with given size and speed
-	#var child = preload("path_to_your_child_scene.tscn").instance()
-	# Initialize the child with combined stats
-	#child.size = size
-	#child.speed = speed
-	# Position the child appropriately, maybe randomly around the parent
-	#child.global_position = global_position + Vector3(rand_range(-1, 1), 0, rand_range(-1, 1))
+func create_child(size,speed,accel):
+	print("Creatign Child")
+	# Create a new instance of the same creature as a child
+	var child = load("res://Scenes/Prey/Desert_Prey.tscn").instantiate()
+	# Initialize child variables
+	child.size = size * child_scale_factor
+	child.speed = speed * child_scale_factor
+	child.accel = accel * child_scale_factor
+	#set speed
+	child.is_child = true  # Mark the child as a child
+	# Position the child nearby the parent
+	child.global_position = global_position + Vector3(randi_range(-1, 1), 0, randi_range(-1, 1))
 	# Add the child to the scene or appropriate container
-	#get_parent().add_child(child)
-
+	get_parent().add_child(child)
 
 func _on_child_timer_timeout():
 	is_child = false
 	size /= child_scale_factor
 	speed /= child_speed_factor
+
+
+func _on_repo_state_processing(delta):
+	pass # Replace with function body.
--- Scripts/Terrian.gd
+++ Scripts/Terrian.gd
@@ -1,60 +1,44 @@
 extends Node3D
-var scene = "Fire"
+
+var scene = "Desert"
 var food_scene = load("res://Scenes/Food/"+ scene + "_Food.tscn")
 var prey_scene = load("res://Scenes/Prey/"+ scene + "_Prey.tscn")
 var pred_scene = load("res://Scenes/Pred/"+ scene + "_Pred.tscn")
 
-
 var food_size = 0.5
-var rabbit_size = 1
 var spawn_rate = 1
 var rabbit_instance
 var enemy_instance
+var food_instance
+var rabbit_instances = []
 
 @onready var map_size = get_node("NavigationRegion3D/CSGBox3D").get_scale()
-#@onready var map_height = get_node("NavigationRegion3D/CSGBox3D")
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-	
 	$Timer.start()
-	
-	pass
-	
 
 func _spawn_food():
-	var food_instance = food_scene.instantiate()
-	var food_instance_scale = Vector3(food_size,food_size,food_size)
+	food_instance = food_scene.instantiate()
 	food_instance.position.x = randf_range(-map_size.x/2.1,map_size.x/2.1)
 	food_instance.position.z = randf_range(-map_size.z/2.1,map_size.z/2.1)
 	food_instance.position.y = map_size.y + food_size/2
 	add_child(food_instance)
-	#print(map_height)
-	food_instance.scale = food_instance_scale
 
 func _spawn_rabbit():
 	rabbit_instance = prey_scene.instantiate()
-	var rabbit_instance_scale = Vector3(rabbit_size,rabbit_size,rabbit_size)
-	#fix the y position to work with tilts
 	rabbit_instance.position.x = randf_range(-map_size.x/2.1,map_size.x/2.1)
 	rabbit_instance.position.z = randf_range(-map_size.z/2.1,map_size.z/2.1)
 	rabbit_instance.position.y = map_size.y + 1
 	add_child(rabbit_instance)
+	rabbit_instances.append(rabbit_instance)
 
-	rabbit_instance.scale = rabbit_instance_scale
 
 func _spawn_enemy():
 	enemy_instance = pred_scene.instantiate()
-	var enemy_instance_scale = Vector3(rabbit_size,rabbit_size,rabbit_size)
-	#fix the y position to work with tilts
 	enemy_instance.position.x = randf_range(-map_size.x/2.1,map_size.x/2.1)
 	enemy_instance.position.z = randf_range(-map_size.z/2.1,map_size.z/2.1)
 	enemy_instance.position.y = 0.1
-	
 	add_child(enemy_instance)
-
-	enemy_instance.scale = enemy_instance_scale
 
 func _physics_process(delta):
 	
@@ -63,18 +47,16 @@ func _physics_process(delta):
 		_spawn_rabbit()
 		_spawn_rabbit()
 		_spawn_rabbit()
-		_spawn_rabbit()
-		_spawn_rabbit()
-		_spawn_enemy()
-		_spawn_enemy()
-		_spawn_enemy()
-		_spawn_enemy()
-		_spawn_enemy()
 		spawn_rate = 0
 		
-	if Input.is_action_just_pressed("Test"):
+	if Input.is_action_just_pressed("SpeedUp"):
 		_spawn_food()
-	
+		# Increase speed of all prey instances
+		for rabbit in rabbit_instances:
+			rabbit.speed *= 2.0
+	if Input.is_action_just_pressed("SpeedDown"):
+		for rabbit in rabbit_instances:
+			rabbit.speed /= 2.0
 	if Input.is_action_just_pressed("Exit"):
 		get_tree().quit()
 
